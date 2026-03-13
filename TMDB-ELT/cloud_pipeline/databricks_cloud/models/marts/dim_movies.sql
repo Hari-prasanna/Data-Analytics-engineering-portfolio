4e66e27727e6b87e7ba378c500699d8187ca9b1e
Hari Prasanna <hariprasanna0116@gmail.com>
2026-03-13 16:12:14 +0100

    Phase 2: migrated staging, marts, test files and tested

---
+++ TMDB-ELT/cloud_pipeline/databricks_cloud/models/marts/dim_movies.sql
@@ -0,0 +1,32 @@
+{{ config(
+    materialized='incremental',
+    unique_key='movie_id'
+) }}
+
+WITH movies AS (
+    SELECT * FROM {{ ref('stg_movies') }}
+),
+
+details AS (
+    SELECT * FROM {{ ref('stg_movie_details') }}
+),
+
+final_dimension AS (
+    SELECT
+        m.movie_id,
+        m.title,
+        m.language,
+        m.release_date,
+        d.runtime_minutes,
+        d.release_status,
+        m.popularity,
+        m.vote_avg,
+        GREATEST(m.loaded_at, d.loaded_at) AS updated_at
+    FROM movies m
+    LEFT JOIN details d ON m.movie_id = d.movie_id
+)
+
+SELECT * FROM final_dimension
+{% if is_incremental() %}
+    WHERE updated_at > (SELECT MAX(updated_at) FROM {{ this }})
+{% endif %}
